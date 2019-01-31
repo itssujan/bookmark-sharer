bfd8f05556658c60c606df63e7f8ec85ff02ae52
Sujan Abraham <itssujan@gmail.com>
2019-01-31 09:11:02 -0500

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    validates_presence_of :first_name
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_presence_of :first_name
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject do
+    described_class.new(first_name: Faker::Name.first_name,
+                        last_name: Faker::Name.last_name,
+                        email: 'john@doe.com')
+  end
 
-    subject { described_class.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "john@doe.com") }
+  describe '#validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
 
-    describe "#validations" do
-      it "is valid with valid attributes" do
-        expect(subject).to be_valid
-      end
+    it 'is not valid without a first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
 
-      it "is not valid without a first_name" do
-        subject.first_name = nil
-        expect(subject).to_not be_valid
-      end
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
 
-      it "is not valid without an email" do
-        subject.email = nil
-        expect(subject).to_not be_valid
-      end
+    it 'should validate the email format' do
+      subject.email = 'foo'
+      expect(subject).to_not be_valid
+    end
 
-      it "should validate the email format" do
-        subject.email = "foo"
-        expect(subject).to_not be_valid
-      end
+    context 'when email is not unique' do
+      it 'is not valid' do
+        user1 = User.new(first_name: Faker::Name.first_name,
+                         last_name: Faker::Name.last_name,
+                         email: 'john@doe.com')
+        user1.save
 
-      context "when email is not unique" do
-          it "is not valid" do
-            user1 = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "john@doe.com")
-            user1.save
+        user2 = User.new(first_name: Faker::Name.first_name,
+                         last_name: Faker::Name.last_name,
+                         email: 'john@doe.com')
+        user2.save
 
-            user2 = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "john@doe.com")
-            user2.save
-
-            expect(user2.errors[:email]).to eq(["has already been taken"])
-          end
-      end
-
-      context "when email is unique" do
-          before { described_class.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "jane@doe.com") }
-          it { expect(subject).to be_valid }
+        expect(user2.errors[:email]).to eq(['has already been taken'])
       end
     end
+
+    context 'when email is unique' do
+      before do
+        described_class.new(first_name: Faker::Name.first_name,
+                            last_name: Faker::Name.last_name,
+                            email: 'jane@doe.com')
+      end
+      it { expect(subject).to be_valid }
+    end
+  end
 end
