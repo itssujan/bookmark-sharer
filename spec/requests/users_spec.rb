c968f36b67b99193e903174cfb894d9f85dc2964
Sujan Abraham <itssujan@gmail.com>
2019-02-01 09:57:44 -0500

    user api first cut

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let!(:users) { create_list(:user, 10) }
+  let(:user_id) { users.first.id }
+
+  describe 'GET /users' do
+    before { get '/users' }
+
+    it 'returns users' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns 200 status code' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
